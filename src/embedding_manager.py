4cf1bf90ccff3ed63747171d12ad89e9662502fe
aslanali <aslanali@users.noreply.github.com>
2024-09-25 20:54:24 +0530

    Update embedding_manager.py

--- src/embedding_manager.py
+++ src/embedding_manager.py
@@ -28,23 +28,3 @@ class EmbeddingManager:
         if os.path.exists(self.file_path):
             return FAISS.load_local(self.file_path, self.embeddings,allow_dangerous_deserialization=True)
         return None
-
-
-# class EmbeddingManager:
-#     def __init__(self, file_path="models/faiss_store_openai.pkl"):
-#         self.file_path = file_path
-#         self.embeddings = OpenAIEmbeddings()
-    
-#     def create_and_save_embeddings(self, docs):
-#         """Creates embeddings and saves them to a FAISS index."""
-#         vectorstore = FAISS.from_documents(docs, self.embeddings)
-#         print(os.getcwd())
-#         with open(self.file_path, "wb") as f:
-#             pickle.dump(vectorstore, f)
-    
-#     def load_embeddings(self):
-#         """Loads the FAISS index from a pickle file."""
-#         if os.path.exists(self.file_path):
-#             with open(self.file_path, "rb") as f:
-#                 return pickle.load(f)
-#         return None
